4701ace2e36f886ab29c2fdaabcf54f4a365c0b4
Syed Wajahat Ali <wajahat.klk25.com>
2022-05-23 20:32:26 +0200

    it4

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,72 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength  = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage (damage) {
+    damage - this.health;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name,health, strength) {
+    super(health, strength)
+    this.name = name;
+  }
+  attack();
+  receiveDamage (damage) {
+    this.health = this.health - damage;
+    if(this.health >= 1){
+      return `${this.name} has received ${damage} points of damage`
+    }else return `${this.name} has died in act of combat`
+  }
+  battleCry () {
+    return 'Odin Owns You All!'
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+    if(this.heath >= 1){
+      return `A Saxon has received ${damage} points of damage`
+    }else if (this.health <1) {
+      return ` A Saxon has died in combat`
+    } 
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor(){
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+  addViking() {
+    const viking1 = new Viking(Regna, 100, 50);
+    viking1.push(this.vikingArmy);
+  }
+  addSaxon(saxon){
+    const saxon1 = new saxon;
+    saxon1.push(this.saxonArmy);
+  }
+  vikingAttack() {
+    let saxon = Saxon.Math.random() * saxon;
+    
+  }
+  saxonAttack()
+  showStatus()
+}
 
 
 
